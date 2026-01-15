9164952f2e21bfb84394b9b6a93f612c12a43e05
Lucas Alixame <lucasali2003@gmail.com>
2026-01-15 10:58:03 -0300

    [FIX] Remove CDN URL dependency in fetchS3MDX function and use direct S3 URL

--- app/docs/[...slug]/page.tsx
+++ app/docs/[...slug]/page.tsx
@@ -36,17 +36,14 @@ async function fetchS3MDX(
     projectSlug: string,
     path: string
 ): Promise<string | null> {
-    const baseUrl = process.env.NEXT_PUBLIC_CDN_URL;
-    if (!baseUrl) return null;
-
-    const url = `${baseUrl}/projects/${projectSlug}/latest/${path}.md`;
-
+    const url = `https://artisaan-docs-bucket.s3.us-east-1.amazonaws.com/projects/${projectSlug}/latest/${path}.md`;
     try {
         const res = await fetch(url, {
             // next: { revalidate: 30 },
         });
 
         if (!res.ok) return null;
+
         return await res.text();
     } catch {
         return null;
@@ -109,7 +106,7 @@ export default async function DocPage({ params }: { params: Promise<{ slug?: str
             <section className="container max-w-[1400px] mx-auto px-6 flex min-h-screen pt-14">
                 <aside className="hidden md:block md:sticky top-14 left-0 z-30 w-full md:w-60 lg:w-[260px] py-6 lg:py-8 pr-6">
                     <ScrollArea className="shrink-0 h-[calc(100vh-3.5rem)] w-full space-y-8">
-                        {manifest.sections.map((section: any, index: number) => (
+                        {!!manifest && manifest.sections.map((section: any, index: number) => (
                             <div className="pb-4" key={index} title={section.title}>
                                 <h4 className="mb-3 text-sm font-semibold tracking-tight text-white">
                                     {section.title}
@@ -151,7 +148,7 @@ export default async function DocPage({ params }: { params: Promise<{ slug?: str
         <section className="container max-w-[1400px] mx-auto px-6 flex min-h-screen pt-14">
             <aside className="hidden md:block md:sticky top-14 left-0 z-30 w-full md:w-60 lg:w-[260px] py-6 lg:py-8 pr-6">
                 <ScrollArea className="shrink-0 h-[calc(100vh-3.5rem)] w-full space-y-8">
-                    {manifest.sections.map((section: any, index: number) => (
+                    {!!manifest && manifest.sections.map((section: any, index: number) => (
                         <div className="pb-4" key={index} title={section.title}>
                             <h4 className="mb-3 text-sm font-semibold tracking-tight text-white">
                                 {section.title}
